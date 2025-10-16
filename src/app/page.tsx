68ea49f73ef352bd480cc400a42b75273b9f5605
greg <gregoryproencajr@gmail.com>
2025-10-16 15:47:32 -0300

    install next

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex h-screen items-center justify-center bg-black">
+      <h1 className="text-white text-6xl font-bold tracking-wide select-none">
+        Gr3gJr
+      </h1>
+    </main>
+  );
+}
